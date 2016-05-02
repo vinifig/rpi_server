33b34439e5efaa042d6795a55cb060a32157d825
vinifig <vinifig@hotmail.com>
2016-05-02 07:29:20 -0300

    Codigo pronto para teste

--- client.js
+++ client.js
@@ -3,12 +3,23 @@
 const SocketIO = require('socket.io-client');
 const Server = process.argv[2];
 const socket = SocketIO(Server);
+const Leds   = require('./lib/led');
 
 const methods = {}
 
+// Test method
 methods.command = function(data){
   console.dir(data);
 }
+
+methods.liga = function(data){
+  Leds.acendeLed(data.id);
+}
+
+method.desliga = function(data){
+  Leds.apagaLed(data.id);
+}
+
 
 function connect(){
   console.log("Conectado com sucesso!");
